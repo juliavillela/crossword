9d83b3f0f5cb1d3179e252af36082e94a67e8de1
juliavillela <julia.arruda@gmail.com>
2024-05-08 15:42:46 -0300

    rmv unsused validate_word_list function

--- src/crosword_generator/generate.py
+++ src/crosword_generator/generate.py
@@ -97,42 +97,6 @@ class CrosswordBuilder:
         else:
             return grid
 
-def validate_word_list(word_list):
-    """
-    Raises an error if word list does not meet any of the criteria
-
-    A word_list is valid if:
-    - it has more than MIN_WORDS and less than MAX_WORDS.
-    - no words in list are either too long or too short
-    - each word has at least one char in common with another word.
-    """
-    # word list is of adequate length
-    if len(word_list) > MAX_WORDS or len(word_list) < MIN_WORDS:
-        raise ValueError(f"word list should have between {MIN_WORDS} and {MAX_WORDS}, not {len(word_list)}")
-
-    # no words are too long
-    too_long = [w for w in word_list if len(w)>MAX_WORD_LEN]
-    if len(too_long):
-        raise ValueError(f"word list contains words that are longer than {MAX_WORD_LEN}: {too_long}")
-    
-    # no words are too short
-    too_short = [w for w in word_list if len(w)<MIN_WORD_LEN]
-    if len(too_short):
-        raise ValueError(f"word list contains words that are shorter than {MIN_WORD_LEN}: {too_short}")
-    
-    # words should have at least one char in common with another word        
-    for i, word1 in enumerate(word_list):
-        found_common = False
-        for j, word2 in enumerate(word_list):
-            if i != j:  # Skip comparing the word with itself
-                if any(char in word2 for char in word1):
-                    found_common = True
-                    break
-        if not found_common:
-            raise ValueError(f"word list is impossible. word '{word1}' has no chars in common with other words")
-    
-    return True
-
 class CrosswordGenerator:
     """
     Use CrosswordBuilder and internal settings to generate valid crossword.
