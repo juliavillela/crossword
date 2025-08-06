164d6722455157324711ac009adf06b271eb96dc
juliavillela <julia.arruda@gmail.com>
2025-08-06 08:20:12 -0300

    update import for iterative crossword generator

--- src/tests/test_generate.py
+++ src/tests/test_generate.py
@@ -1,11 +1,11 @@
 import pytest
 from crosword_generator.constants import *
-from crosword_generator.generate import CrosswordGenerator
+from crosword_generator.generate import IterativeCrosswordGenerator
 from crosword_generator.crossword import Crossword
 from crosword_generator.word_placement_grid import WordPlacementGrid
 
 def test_initialization():
-    generator = CrosswordGenerator(["bat","cats"])
+    generator = IterativeCrosswordGenerator(["bat","cats"])
 
     # make sure words are sorted by descending length
     assert generator.words == ["cats", "bat"]
@@ -18,7 +18,7 @@ def test_attempt_build_grid_returns_WordPlacementGrid():
     wordlist = [
         "aaa", "aaaa", "aaaaa"
     ]
-    generator = CrosswordGenerator(wordlist)
+    generator = IterativeCrosswordGenerator(wordlist)
     result = generator._attempt_grid_build()
     # make sure result is an instance of WordPlacementGrid
     assert isinstance(result, WordPlacementGrid)
@@ -31,7 +31,7 @@ def test_retry_grid_builds_returns_WordPlacementGrid():
     wordlist = [
         "aaa", "aaaa", "aaaaa"
     ]
-    generator = CrosswordGenerator(wordlist)
+    generator = IterativeCrosswordGenerator(wordlist)
     result = generator._retry_grid_builds()
 
     # make sure result is an instance of WordPlacementGrid
@@ -45,7 +45,7 @@ def test_generate_returns_Crossword():
     wordlist = [
         "aaa", "aaaa", "aaaaa"
     ]
-    generator = CrosswordGenerator(wordlist)
+    generator = IterativeCrosswordGenerator(wordlist)
     result = generator.generate()
     assert isinstance(result, Crossword)
 
@@ -54,7 +54,7 @@ def test_attempt_build_grid_returns_incomplete_grid_for_impossible_word_list():
     wordlist = [
         "aaa", "bbb", "ccc"
     ]
-    generator = CrosswordGenerator(wordlist)
+    generator = IterativeCrosswordGenerator(wordlist)
     result = generator._attempt_grid_build()
     
     # make sure result is an instance of WordPlacementGrid
@@ -68,7 +68,7 @@ def test_retry_grid_builds_returns_none_for_impossible_word_list():
     wordlist = [
         "aaa", "bbb", "ccc"
     ]
-    generator = CrosswordGenerator(wordlist)
+    generator = IterativeCrosswordGenerator(wordlist)
     result = generator._retry_grid_builds()
 
     assert result == None
@@ -77,7 +77,7 @@ def test_generate_returns_none_for_impossible_word_list():
     wordlist = [
         "aaa", "bbb", "ccc"
     ]
-    generator = CrosswordGenerator(wordlist)
+    generator = IterativeCrosswordGenerator(wordlist)
     result = generator.generate()
 
     assert result == None
